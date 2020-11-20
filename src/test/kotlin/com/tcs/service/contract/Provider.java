8e2146b07a6aa28ff8144002827a7b3d30c240c8
raghavG2213 <raghav.3122@gmail.com>
2020-11-20 17:54:28 +0530

    after provider change

--- src/test/kotlin/com/tcs/service/contract/Provider.java
+++ src/test/kotlin/com/tcs/service/contract/Provider.java
@@ -1,4 +1,4 @@
-package com.tcs.services.contract;
+package com.tcs.service.contract;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.kotlin.KotlinModule;
